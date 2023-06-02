8cb169fa98af24bae9f51c74d140afeff9ef72e7
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2023-06-02 12:15:22 -0700

    Remove starter code and update descriptions in SocialMediaController

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -17,12 +17,6 @@ import Model.Message;
 import Service.SocialMediaService;
 import Util.ConnectionUtil;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your 
- * controller. The endpoints you will need can be found in readme.md as well as 
- * the test cases. You should refer to prior mini-project labs and lecture 
- * materials for guidance on how a controller may be built.
- */
 public class SocialMediaController {
 
     private static final SocialMediaService SOCIAL_MEDIA_SERVICE = 
@@ -31,9 +25,7 @@ public class SocialMediaController {
         new MessageDaoH2(ConnectionUtil.getConnection()));
 
     /**
-     * In order for the test cases to work, you will need to write the endpoints 
-     * in the startAPI() method, as the test suite must receive a Javalin object 
-     * from this method.
+     * Starts the API using Javalin.
      * 
      * @return a Javalin app object which defines the behavior of the Javalin 
      * controller.
@@ -41,7 +33,6 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
 
-        app.get("example-endpoint", this::exampleHandler);
         app.post("/register", this::addAccountHandler);
         app.post("/login", this::loginAccountHandler);
         app.post("/messages", this::createMessageHandler);
@@ -51,22 +42,14 @@ public class SocialMediaController {
     }
 
     /**
-     * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both 
-     * the HTTP request and response.
-     */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
-    }
-
-    /**
      * Takes an account without an ID and adds it to the Account database table.
      * An account with an ID is returned thru the API.
      * If the account already exists or if the username or password is not acceptable, then a HTTP response code of
      *  400 is returned to the client.
      * If there is an issue interacting with the database, a HTTP response code of 500 is returned to the client.
      * 
-     * @param context The context that contains an account without an ID, in JSON.
+     * @param context Contains an account without an ID, in JSON, from the client.  Sends back the newly registered
+     *  account with an ID, username, and password, in JSON.
      */
     private void addAccountHandler(Context context) {
         Account unregisteredAccount = context.bodyAsClass(Account.class);
@@ -88,7 +71,8 @@ public class SocialMediaController {
      * If the password is incorrect or if the account does not exist, a HTTP response code of 401 is returned.
      * If there is an issue interacting with the database, a HTTP response code of 500 is returned to the client.
      * 
-     * @param context Contains an account in JSON that has username and password only.
+     * @param context Contains an account in JSON that has username and password only.  Sends back the correct matching
+     *  account with ID, username, and password, in JSON.
      */
     private void loginAccountHandler(Context context) {
         Account account = context.bodyAsClass(Account.class);
@@ -110,7 +94,8 @@ public class SocialMediaController {
      * If the message text is empty or is too long, or if the poster does not have an account, then a HTTP response code
      *  of 400 is returned.
      * 
-     * @param context Contains a Message object in JSON, only without a message ID.
+     * @param context Contains a Message object in JSON, only without a message ID.  Sends back a Message object with
+     *  the same fields but with ID included, in JSON.
      */
     private void createMessageHandler(Context context) {
         Message message = context.bodyAsClass(Message.class);
